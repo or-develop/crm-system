e3a322406c3b575ac9298e8bd7e2e58dbf8f02bc
Alex Larin <alex.larin13@yandex.ru>
2022-04-25 10:27:18 +0300

    add output list of Lead

--- crm/serializers.py
+++ crm/serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 from .models import Lead
 
 
-class AddLeadSerializer(serializers.ModelSerializer):
-    """ API для добавления Лида в базу данных. """
+class LeadSerializer(serializers.ModelSerializer):
+    """ Сериализатор для мождели Lead """
 
     class Meta:
         model = Lead
--- crm/views.py
+++ crm/views.py
@@ -2,11 +2,18 @@ from rest_framework import generics
 
 from .models import Lead
 from .services import get_all
-from .serializers import AddLeadSerializer
+from .serializers import LeadSerializer
 
 
 class AddLead(generics.CreateAPIView):
     """ Класс добавления Лида в базу. """
 
     queryset = get_all(Lead)
-    serializer_class = AddLeadSerializer
+    serializer_class = LeadSerializer
+
+
+class LeadList(generics.ListAPIView):
+    """ Класс вывода списка всех Лидов"""
+
+    queryset = get_all(Lead)
+    serializer_class = LeadSerializer
