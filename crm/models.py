91869d0aa9db2708ca72012afcb5796e35e71a49
Sergey <sergey.lyutavin22@mail.ru>
2022-05-05 22:41:49 +0300

    Funnel model

--- crm/models.py
+++ crm/models.py
@@ -51,9 +51,9 @@ class Agent(AbstractUser):
         db_table = 'agents'
 
 
-class Funnel:
+class Funnel(models.Model):
     """" Модель воронки продаж. """
-    title = models.CharField('Категория воронки', max_length=256)
+    title = models.CharField('Название воронки', max_length=256)
 
     def __str__(self):
         return self.title
@@ -73,8 +73,8 @@ class Lead(AbstractModelDateTime):
     email = models.EmailField('Email клиента', max_length=256)
     phone = models.CharField('Номер телефона клиента',
                              max_length=50, blank=True)
-    funnel = models.OneToOneField('Текущая категория воронки',
-                                  Funnel, on_delete=models.CASCADE, related_name='lead')
+    funnel = models.ForeignKey('Текущая категория воронки',
+                               Funnel, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
 
